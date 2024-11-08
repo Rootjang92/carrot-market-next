89bdb2eac63460bcf193b0ae4413e5f589f5dc5c
Rootjang92 <janggeunho7@gmail.com>
2024-11-09 08:33:08 +0900

    Feat: form input, button component

---
+++ components/form-button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  text: string;
+  loading: boolean;
+}
+
+export default function FormButton({ text, loading }: Props) {
+  return (
+    <button
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+      disabled={loading}
+    >
+      {loading ? '로딩 중' : text}
+    </button>
+  );
+}
---
+++ components/form-input.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+
+export default function FormInput({ type, placeholder, required, errors }: Props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="bg-transparent rounded-md w-full h-10 transition focus:outline-none ring-1 focus:ring-4 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error) => (
+        <span className="text-red-500 font-medium" key={error}>
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
