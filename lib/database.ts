ef3ace61845a5f2da51fc992c11c6055bcab71fe
Rootjang92 <janggeunho7@gmail.com>
2025-12-04 15:24:45 +0900

    불필요한 수정 제거

--- lib/database.ts
+++ lib/database.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const db = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const db = new PrismaClient();
 
 export default db;
