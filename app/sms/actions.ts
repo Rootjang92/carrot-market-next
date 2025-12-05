54bd242c1eef5de0924dd6dd8feaf1ab4d4c2f61
Rootjang92 <janggeunho7@gmail.com>
2025-12-05 09:15:06 +0900

    SMS 연락처 db 비교 로직

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -3,9 +3,27 @@
 import { z } from 'zod';
 import validator from 'validator';
 import { redirect } from 'next/navigation';
+import crypto from 'crypto';
+
+import db from '@/lib/database';
 
 interface ActionState {
   token: boolean;
+}
+
+async function getToken(): Promise<string> {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (exists) return getToken();
+  else return token;
 }
 
 const phoneSchema = z
@@ -26,6 +44,34 @@ export async function smsVerification(prevState: ActionState, formData: FormData
         error: result.error.flatten(),
       };
     } else {
+      // 이전 토큰 삭제 -> 토큰 생성 -> twilio로 토큰 전송
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+
+      const token = await getToken();
+
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString('hex'),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+
       return { token: true };
     }
   } else {
