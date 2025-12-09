02326625b941e28170321768101e9c853115782e
Rootjang92 <janggeunho7@gmail.com>
2025-12-09 09:51:42 +0900

    Product component 추가, Product 임시 db, Product detail page layout

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,13 +1,29 @@
+import db from '@/lib/database';
+
+import ListProduct from '@/components/list-product';
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+
+  return products;
 }
 
 export default async function Chat() {
   const products = await getProducts();
 
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
