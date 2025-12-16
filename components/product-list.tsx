cc3585d6f0206e349f4bf36979eccf400f9ac7d8
Rootjang92 <janggeunho7@gmail.com>
2025-12-16 09:44:48 +0900

    무한 스크롤 연습

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+
+import { useState, useRef, useEffect } from 'react';
 
 import ListProduct from './list-product';
 import { InitialProducts } from '@/app/(tabs)/products/page';
@@ -15,18 +16,38 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
 
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
-    }
+  const trigger = useRef<HTMLSpanElement>(null);
 
-    setIsLoading(false);
-  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+        const element = entries[0];
+
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+      },
+    );
+
+    if (trigger.current) observer.observe(trigger.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
 
   return (
     <div className="p-5 flex flex-col gap-5">
@@ -36,13 +57,15 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {isLastPage ? (
         'No more items'
       ) : (
-        <button
-          onClick={onLoadMoreClick}
-          disabled={isLoading}
-          className="text-sm font-semibold bg-orange-500 text-white w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        <span
+          ref={trigger}
+          style={{
+            marginTop: `${(page + 1) * 900}vh`,
+          }}
+          className="mb-96 text-sm font-semibold bg-orange-500 text-white w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? '로딩 중' : 'Load more'}
-        </button>
+        </span>
       )}
     </div>
   );
